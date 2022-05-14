e2b6b5fab3bd48105bf4370a3e88cb13acb901cf
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-14 13:48:21 -0300

    Update modal.js

--- src/commands/tests/modal.js
+++ src/commands/tests/modal.js
@@ -1,1 +1,16 @@
-
+const Command = require('../../structures/Command')
+const Discord = require('discord.js');
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            description: 'Mostra o ping do bot.'
+        })
+    }
+    run = (interaction) => {
+     const modal = new Discord.Modal()
+      .setTitle("Test Modal")
+      .setCustomId("1");
+      interaction.reply({content: "Test", components: [row]})
+    }
+}
