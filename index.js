b27f4395a8a5a3dce74618383745de31a7f2158e
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-14 16:24:54 +0000

    t

--- index.js
+++ index.js
@@ -16,44 +16,12 @@ client.login(process.env.token)
 
 
 const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = requirdiscord-interactions')');
 
 const app = express();
 
 app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), (req, res) => {
-  const interaction = req.body;
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    res.send({
-      type: InteractionResponseType.APPLICATION_MODAL,
-      data: {
-        title: 'Test',
-        custom_id: 'test-modal',
-        components: [
-          {
-            type: 1,
-            components: [
-              {
-                type: 4,
-                style: 1,
-                label: 'Short Input',
-                custom_id: 'short-input',
-                placeholder: 'Short Input',
-              },
-            ],
-          },
-          {
-            type: 1,
-            components: [
-              {
-                type: 4,
-                style: 1,
-                label: 'Paragraph Input',
-                custom_id: 'paragraph-input',
-                placeholder: 'Paragraph Input',
-                required: false,
-              },
-            ],
-          },
+  const interaction = req.console.log(interaction)      },
         ],
       },
     });
