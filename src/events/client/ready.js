dbcdd4060ccfcae8f778217b0b3ebdd0c5741ad1
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-14 12:17:58 -0300

    Create ready.js

---
+++ src/events/client/ready.js
@@ -0,0 +1,13 @@
+const Event = require('../../structures/Event')
+
+module.exports = class extends Event {
+    constructor(client) {
+        super(client, {
+            name: 'ready'
+        })
+    }
+    run = async () => {
+        console.log(`Bot ${this.client.user.username} logado com sucesso em ${this.client.guilds.cache.size} servidores.`)
+        this.client.registryCommands()
+    }
+}
