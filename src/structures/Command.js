7093bac3e53ea91cedddaf0e82dc9d37a1968983
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-14 12:15:17 -0300

    Create Command.js

---
+++ src/structures/Command.js
@@ -0,0 +1,9 @@
+class Command {
+    constructor(client, options) {
+        this.client = client
+        this.name = options.name
+        this.description = options.description
+        this.options = options.options
+        this.requireDatabase = options.requireDatabase
+    }
+}
