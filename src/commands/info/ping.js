e4d820bbcc4b45e8a96573b7820284583fe76568
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-14 12:23:41 -0300

    Create ping.js

---
+++ src/commands/info/ping.js
@@ -0,0 +1,16 @@
+const Command = require('../../structures/Command')
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            description: 'Mostra o ping do bot.'
+        })
+    }
+    run = (interaction) => {
+        interaction.reply({
+            content: `Atual ping de Ayu é \`${this.client.ws.ping}\`ms.`,
+            ephemeral: true
+        })
+    }
+}
