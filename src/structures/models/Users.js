102d725a966171302fb46c76cf6ffcfb40ce4fc9
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-15 15:03:46 -0300

    Create Users.js

---
+++ src/structures/models/Users.js
@@ -0,0 +1,13 @@
+const Schema = mongo.Schema;
+
+const User = new Schema({
+	_id: String,
+	about: String,
+	economy: {
+		coins: Number,
+	},
+	lang: { type: String, default: "pt-br", required: true }
+});
+
+
+module.exports = new mongo.model("Users", User)
