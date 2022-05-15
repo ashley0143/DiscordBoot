9baca4b879f969fb5edd977f010f06c5f12034ee
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-15 15:12:47 -0300

    Update interactionCreate.js

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,4 +1,5 @@
 const Event = require('../../structures/Event')
+const User = require('../../structures/models/Users')
 
 module.exports = class extends Event {
     constructor(client) {
@@ -8,6 +9,16 @@ module.exports = class extends Event {
     }
     run = async (interaction) => {
         if (interaction.isCommand()) {
+          const user = await User.findOne({_id: interaction.member.id});
+          if(!user) {
+           new User({_id: interaction.member.id,
+			lang: "pt-br",
+			about: "Olá, parece que ainda não tenho um sobre mim!",
+			economy: {
+				coins: 500
+			}
+           }).save()
+          }
           const cmd = this.client.commands.find(c => c.name === interaction.commandName)
             if (cmd) {
              cmd.run(interaction)
