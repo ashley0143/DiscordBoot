4831ddd7455c2581f9749ddca07de8c9398bac7e
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-16 18:14:41 -0300

    Update work.js

--- src/commands/economy/work.js
+++ src/commands/economy/work.js
@@ -1,6 +1,7 @@
 const Command = require('../../structures/Command')
 const Users = require('../../structures/models/Users')
-
+const cooldown = new Set();
+const cooldownTime = 30000; 
 module.exports = class extends Command {
     constructor(client) {
         super(client, {
@@ -11,8 +12,6 @@ module.exports = class extends Command {
     run = async function(interaction) {
         const user = await Users.findOne({_id: interaction.member.id});
         if(!user) return interaction.reply("[Erro] Usuário não existe.");
-        const cooldown = new Set();
-        const cooldownTime = 30000; 
         if(cooldown.has(interaction.user.id)) return interaction.reply({content:"😪 • Uff, você trabalhou muito, porque não descansa um pouco?", type: 'EPHEMERAL'});
         var random = Math.floor(Math.random(500) * 50000)
         user.economy.coins += random;
