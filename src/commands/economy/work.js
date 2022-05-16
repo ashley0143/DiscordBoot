3aea9832d0b3a05749c2530fe5759225a914a8db
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-16 18:09:08 -0300

    Create work.js

---
+++ src/commands/economy/work.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/Command')
+const Users = require('../../structures/models/Users')
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'work',
+            description: 'Work to earn money!'
+        })
+    }
+    run = async function(interaction) {
+        const user = await Users.findOne({_id: interaction.member.id});
+        if(!user) return interaction.reply("[Erro] Usuário não existe.");
+        const cooldown = new Set();
+        const cooldownTime = 30000; 
+        if(cooldown.has(interaction.user.id)) return interaction.reply({content:"😪 • Uff, você trabalhou muito, porque não descansa um pouco?", type: 'EPHEMERAL'});
+        var random = Math.floor(Math.random(500) * 50000)
+        user.economy.coins += random;
+        user.save();
+        interaction.reply(`👷 • Você trabalhou e conseguiu $${random}!`)
+        cooldown.add(interaction.user.id);
+        setTimeout(() => {
+          cooldown.delete(interaction.user.id);
+        }, cooldownTime);
+        }
+}
