f2c89c4c3cc078801e941da57d33ae6b00d79591
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-16 21:23:33 -0300

    Create language.js

---
+++ src/commands/settings/language.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/Command')
+const Users = require('../../structures/models/Users')
+
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'language',
+            description: 'Altere idioma do bot para seu cliente.',
+            options: [{
+             name: 'language',
+             type: "STRING",
+             description:'Idioma para ser alterado',
+             required:true,
+             choices: [{name: "Portugues Brasileiro", value: "pt-br"}, {name: "Inglês Americano", value: "en-us"}]
+            }]
+        })
+    }
+    run = async (interaction) => {
+        const user = await Users.findOne({_id: interaction.member.id});
+        user.lang = interaction.options.getString('lang');
+        user.save()
+        const lang = this.client.lang({lang: user.lang, cmd: 'language'})
+        interaction.reply(lang.text)
+    }
+}
